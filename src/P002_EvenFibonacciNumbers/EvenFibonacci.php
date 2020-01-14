36c4f24608520b084608d2db9daf74d09b4923e6
Adam Góra <adamgora@icloud.com>
2020-01-14 19:40:34 +0100

    Implemented sum method

--- src/P002_EvenFibonacciNumbers/EvenFibonacci.php
+++ src/P002_EvenFibonacciNumbers/EvenFibonacci.php
@@ -6,5 +6,23 @@ namespace App\P002_EvenFibonacciNumbers;
 
 class EvenFibonacci
 {
+    public function sum(int $termsCount): int
+    {
+        $sum = 0;
+        $prev = 0;
+        $next = 1;
 
+        for ($i = 0; $i < $termsCount; ++$i) {
+            $fibonacciNumber = $prev + $next;
+
+            if($fibonacciNumber % 2 === 0) {
+                $sum += $fibonacciNumber;
+            }
+
+            $prev = $next;
+            $next = $fibonacciNumber;
+        }
+
+        return $sum;
+    }
 }
