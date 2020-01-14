c4cc95d6d0661ef7a4014a6ac1dd95da3e952bcb
Adam Góra <adamgora@icloud.com>
2020-01-14 19:31:47 +0100

    Added test cases

--- tests/P002_EvenFibonacciNumbers/EvenFibonacciTest.php
+++ tests/P002_EvenFibonacciNumbers/EvenFibonacciTest.php
@@ -5,5 +5,23 @@ use PHPUnit\Framework\TestCase;
 
 class EvenFibonacciTest extends TestCase
 {
+    /**
+     * @param int $termsCount
+     * @param int $expectedResult
+     * @dataProvider cases
+     */
+    public function testCalculateSum(int $termsCount, int $expectedResult)
+    {
 
+    }
+
+    public function cases(): array
+    {
+        return [
+            [10, 44],
+            [18, 3382],
+            [23, 60696],
+            [43, 350704366],
+        ];
+    }
 }
