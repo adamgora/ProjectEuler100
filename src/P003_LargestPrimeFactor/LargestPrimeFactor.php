84d255dad529d09fb7a889d7fa930de1a9c0a32c
Adam Góra <adamgora@icloud.com>
2020-01-17 17:37:19 +0100

    Implemented largest prime factor calculator

--- src/P003_LargestPrimeFactor/LargestPrimeFactor.php
+++ src/P003_LargestPrimeFactor/LargestPrimeFactor.php
@@ -6,6 +6,18 @@ class LargestPrimeFactor
 {
     public function get(int $number): int
     {
+        $primeFactors = $this->getPrimeFactorization($number);
+        return max($primeFactors);
+    }
 
+    private function getPrimeFactorization(int $number): array
+    {
+        for ($i = 2; $i < $number; ++$i) {
+            if ($number % $i === 0) {
+                return array_merge([$i], $this->getPrimeFactorization($number / $i));
+            }
+        }
+
+        return [$i];
     }
 }
