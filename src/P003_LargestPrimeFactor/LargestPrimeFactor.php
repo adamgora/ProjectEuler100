37b282c9f7bf6768af0ad7e3fcca14fe8d7b7495
Adam Góra <adamgora@icloud.com>
2020-01-17 17:31:47 +0100

    Created basic structure for class and tests

---
+++ src/P003_LargestPrimeFactor/LargestPrimeFactor.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\P003_LargestPrimeFactor;
+
+class LargestPrimeFactor
+{
+    public function get(int $number): int
+    {
+
+    }
+}
---
+++ tests/P003_LargestPrimeFactor/LargestPrimeFactorTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\P003_LargestPrimeFactor\LargestPrimeFactor;
+use PHPUnit\Framework\TestCase;
+
+class LargestPrimeFactorTest extends TestCase
+{
+
+    public function testGet()
+    {
+
+    }
+}
