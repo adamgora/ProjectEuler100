5eff5dde193234e8270a5df69ba24c30f004bee1
Adam Góra <adamgora@icloud.com>
2020-02-01 09:10:09 +0100

    Solved problem 6

--- src/P006_SumSquareDifference/SumSquareDifference.php
+++ src/P006_SumSquareDifference/SumSquareDifference.php
@@ -6,8 +6,11 @@ namespace App\P006_SumSquareDifference;
 
 class SumSquareDifference
 {
-    public function get(int $limit): int
+    public function get(int $num): int
     {
+        $squareOfTheSum = (($num * ($num + 1)) / 2) ** 2;
+        $sumOfSquares = ($num * ($num + 1) * (2*$num + 1)) / 6;
 
+        return $squareOfTheSum - $sumOfSquares;
     }
 }
