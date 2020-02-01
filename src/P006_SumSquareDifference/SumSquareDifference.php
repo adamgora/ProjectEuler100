03525c5b5710947e32197750ccfa8f45eda62a31
Adam Góra <adamgora@icloud.com>
2020-02-01 08:49:12 +0100

    Created base class and test class

---
+++ src/P006_SumSquareDifference/SumSquareDifference.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\P006_SumSquareDifference;
+
+class SumSquareDifference
+{
+    public function sum(int $limit): int
+    {
+
+    }
+}
---
+++ tests/P006_SumSquareDifference/SumSquareDifferenceTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+use App\P006_SumSquareDifference\SumSquareDifference;
+use PHPUnit\Framework\TestCase;
+
+class SumSquareDifferenceTest extends TestCase
+{
+
+    public function testSum()
+    {
+
+    }
+}
