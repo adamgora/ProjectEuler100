824d1480871075bfb3b1404c10655e320a2b0484
Adam Góra <adamgora@icloud.com>
2020-02-08 09:08:32 +0100

    Created base class, test class and readme file

---
+++ src/P007_1001stPrime/NthPrimeCalculator.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace App\P007_1001stPrime;
+
+class NthPrimeCalculator
+{
+
+}
---
+++ tests/P007_1001stPrime/NthPrimeCalculatorTest.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+use App\P007_1001stPrime\NthPrimeCalculator;
+use PHPUnit\Framework\TestCase;
+
+class NthPrimeCalculatorTest extends TestCase
+{
+
+}
