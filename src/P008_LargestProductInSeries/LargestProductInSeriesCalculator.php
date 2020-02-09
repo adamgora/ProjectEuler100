b908bd649342cde1a934413d67711c22311d7c60
Adam Góra <adamgora@icloud.com>
2020-02-09 16:35:07 +0100

    Created base class and test class

---
+++ src/P008_LargestProductInSeries/LargestProductInSeriesCalculator.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\P008_LargestProductInSeries;
+
+class LargestProductInSeriesCalculator
+{
+    public function calculate(array $series, int $digits): int
+    {
+
+    }
+}
---
+++ tests/P008_LargestProductInSeries/LargestProductInSeriesCalculatorTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+use App\P008_LargestProductInSeries\LargestProductInSeriesCalculator;
+use PHPUnit\Framework\TestCase;
+
+class LargestProductInSeriesCalculatorTest extends TestCase
+{
+
+    public function testCalculate()
+    {
+
+    }
+}
