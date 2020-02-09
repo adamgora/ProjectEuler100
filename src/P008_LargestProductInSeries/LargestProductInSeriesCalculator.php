7dfb83ef192b60cb938b25f46f138cb08e4e3d90
Adam Góra <adamgora@icloud.com>
2020-02-09 16:50:39 +0100

    Finised solution

--- src/P008_LargestProductInSeries/LargestProductInSeriesCalculator.php
+++ src/P008_LargestProductInSeries/LargestProductInSeriesCalculator.php
@@ -7,6 +7,18 @@ class LargestProductInSeriesCalculator
 {
     public function calculate(array $series, int $digits): int
     {
+        $result = 0;
+        $currentIndex = 0;
+        $maxIndex = count($series) + 1 - $digits;
 
+        while ($currentIndex < $maxIndex) {
+            $product = array_product(array_slice($series, $currentIndex, $digits));
+            if($product > $result) {
+                $result = $product;
+            }
+            $currentIndex++;
+        }
+
+        return $result;
     }
 }
