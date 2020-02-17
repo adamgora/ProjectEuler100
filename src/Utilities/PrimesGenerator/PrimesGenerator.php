2bb0ca1d455762f1a2594f88e2d59f8d86c76f5a
Adam Góra <adamgora@icloud.com>
2020-02-17 20:12:01 +0100

    Created class primes generator to hold logic for creating array of primes

---
+++ src/Utilities/PrimesGenerator/PrimesGenerator.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Utilities\PrimesGenerator;
+
+class PrimesGenerator
+{
+    public function generate(int $howMany): array
+    {
+        return [];
+    }
+
+    public function generateUpTo(int $upTo): array
+    {
+        return [];
+    }
+
+
+}
