1583c65b92926021a4e505c452edcf8a7b234ff7
Adam Góra <adamgora@icloud.com>
2020-02-17 20:06:44 +0100

    Created necessary classes

---
+++ src/P010_SummationOfPrimes/PrimesSummarizer.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\P010_SummationOfPrimes;
+
+class PrimesSummarizer
+{
+    public function summarize(int $upTo): int
+    {
+        return 0;
+    }
+}
---
+++ tests/P010_SummationOfPrimes/PrimesSummarizerTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+use App\P010_SummationOfPrimes\PrimesSummarizer;
+use PHPUnit\Framework\TestCase;
+
+class PrimesSummarizerTest extends TestCase
+{
+
+    public function testSummarize()
+    {
+
+    }
+}
