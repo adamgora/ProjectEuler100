a7018de67d744ec6d1f3b9fdac838e212402f6de
Adam Góra <adamgora@icloud.com>
2020-02-17 20:09:31 +0100

    Created test method

--- tests/P010_SummationOfPrimes/PrimesSummarizerTest.php
+++ tests/P010_SummationOfPrimes/PrimesSummarizerTest.php
@@ -6,9 +6,34 @@ use PHPUnit\Framework\TestCase;
 
 class PrimesSummarizerTest extends TestCase
 {
+    /**
+     * @var PrimesSummarizer
+     */
+    private PrimesSummarizer $summarizer;
 
-    public function testSummarize()
+    protected function setUp(): void
     {
+        parent::setUp();
+        $this->summarizer = new PrimesSummarizer();
+    }
 
+    /**
+     * @param int $upTo
+     * @param int $expectedResult
+     * @dataProvider cases
+     */
+    public function testSummarize(int $upTo, int $expectedResult)
+    {
+        $this->assertEquals($this->summarizer->summarize($upTo), $expectedResult);
+    }
+
+    public function cases()
+    {
+        return [
+            [17, 41],
+            [2001, 277050],
+            [140759, 873608362],
+            [2000000, 142913828922],
+        ];
     }
 }
