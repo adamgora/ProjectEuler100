3857050cab46948666d2345fdde4b0370588d3bc
Adam Góra <adamgora@icloud.com>
2020-02-22 12:13:19 +0100

    Rewritten NthPrimeCalculator to use Sieve of Eratosthenes algorithm

--- src/P007_10001stPrime/NthPrimeCalculator.php
+++ src/P007_10001stPrime/NthPrimeCalculator.php
@@ -8,10 +8,53 @@ use App\Utilities\PrimesGenerator;
 
 class NthPrimeCalculator
 {
+    private array $numbers = [];
+    private int $max;
+
     public function calculate(int $num): int
     {
-        $primes = (new PrimesGenerator())->generate($num);
+        $this->setMaxApproximate($num);
+        $this->setNumbersArray();
+        $this->sieveForPrimes();
 
-        return $primes[count($primes) - 1];
+        return $this->returnNthPrime($num);
+    }
+
+    private function setNumbersArray(): void
+    {
+        $this->numbers = array_fill(2, $this->max, true);
+    }
+
+    private function setMaxApproximate(int $num): void
+    {
+        $this->max = (int) floor((log($num) + log(log($num - 1))) * $num) - 1;
+    }
+
+    private function sieveForPrimes(): void
+    {
+        for ($i = 2, $iMax = sqrt($this->max); $i < $iMax; $i++) {
+            if ($this->numbers[$i]) {
+                $this->setMultipliesOfPrimeAsFalse($i);
+            }
+        }
+    }
+
+    /**
+     * @param int $prime
+     */
+    private function setMultipliesOfPrimeAsFalse(int $prime): void
+    {
+        for ($i = $prime ** 2, $j = 1; $i <= $this->max; $i = $prime ** 2 + $j * $prime, $j++) {
+            $this->numbers[$i] = false;
+        }
+    }
+
+    private function returnNthPrime(int $num): int
+    {
+        $numbers = array_keys(
+            array_filter($this->numbers, fn(bool $element) => $element)
+        );
+
+        return $numbers[$num -1];
     }
 }
