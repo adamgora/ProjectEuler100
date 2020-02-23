11b96361895f085b4aeadf0105f66fc6a150b98f
Adam Góra <adamgora@icloud.com>
2020-02-23 07:50:09 +0100

    Written test class

--- tests/P011_LargestProductInGrid/GridProductFinderTest.php
+++ tests/P011_LargestProductInGrid/GridProductFinderTest.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 use App\P011_LargestProductInGrid\GridProductFinder;
@@ -6,9 +7,37 @@ use PHPUnit\Framework\TestCase;
 
 class GridProductFinderTest extends TestCase
 {
+    private GridProductFinder $finder;
 
-    public function testFind()
+    protected function setUp(): void
     {
+        parent::setUp();
+        $this->finder = new GridProductFinder();
+    }
 
+    /**
+     * @param array $grid
+     * @param int $expectedResult
+     * @dataProvider cases
+     */
+    public function testFind(array $grid, int $expectedResult)
+    {
+        $this->assertEquals($expectedResult, $this->finder->find($grid));
+    }
+
+    public function cases(): array
+    {
+        return [
+            [
+                [
+                    [40, 17, 81, 18, 57],
+                    [74, 4, 36, 16, 29],
+                    [36, 42, 69, 73, 45],
+                    [51, 54, 69, 16, 92],
+                    [7, 97, 57, 32, 16],
+                ],
+                14169081,
+            ],
+        ];
     }
 }
