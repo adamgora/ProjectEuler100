86ca3fd8118a09b8b3b85fa90e5d4490ebfa2124
Adam Góra <adamgora@icloud.com>
2020-02-23 07:47:22 +0100

    Created main class and test class

---
+++ src/P011_LargestProductInGrid/GridProductFinder.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\P011_LargestProductInGrid;
+
+class GridProductFinder
+{
+    public function find(array $grid): int
+    {
+        return 0;
+    }
+}
---
+++ tests/P011_LargestProductInGrid/GridProductFinderTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+use App\P011_LargestProductInGrid\GridProductFinder;
+use PHPUnit\Framework\TestCase;
+
+class GridProductFinderTest extends TestCase
+{
+
+    public function testFind()
+    {
+
+    }
+}
