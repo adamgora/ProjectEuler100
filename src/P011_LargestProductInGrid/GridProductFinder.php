eaffd8784f7d1cb0230b77a64808cd2852831ae0
Adam Góra <adamgora@icloud.com>
2020-02-23 08:35:41 +0100

    Added basic methods to search in each row and column

--- src/P011_LargestProductInGrid/GridProductFinder.php
+++ src/P011_LargestProductInGrid/GridProductFinder.php
@@ -8,6 +8,39 @@ class GridProductFinder
 {
     public function find(array $grid, int $numbers): int
     {
-        return 0;
+        $rows = count($grid);
+        $cols = count($grid[0]);
+        //$offset = $numbers;
+        $offset = 2;
+        $product = 0;
+
+        //find product in each row
+//        foreach ($grid as $row) {
+//            for ($i = 0; $i + $offset <= $cols; $i++) {
+//                $tempProduct = array_product(array_slice($row, $i, $offset));
+//                if ($tempProduct > $product) {
+//                    $product = $tempProduct;
+//                }
+//            }
+//        }
+
+        //find product in each column
+        $flatArray = call_user_func_array('array_merge', $grid);
+        $startKeys = array_map(fn($element) => $element * ($offset + 1), range(0, $offset - 1));
+
+        for ($i = 0, $loopsMax = sizeof($flatArray) -1; $i + $offset < $loopsMax; $i++) {
+            $tempProduct = 1;
+            foreach ($startKeys as $startKey) {
+                $tempProduct *= $flatArray[$startKey];
+            }
+
+            if($tempProduct > $product) {
+                $product = $tempProduct;
+            }
+
+            $startKeys = array_map(fn($element) => $element + 1, $startKeys);
+        }
+
+        return $product;
     }
 }
