6f6831ac64daa396a3bf0c072450848e4abf63c8
Adam Góra <adamgora@icloud.com>
2020-02-29 13:09:04 +0100

    Base version (searching in columns and rows)

--- src/P011_LargestProductInGrid/GridProductFinder.php
+++ src/P011_LargestProductInGrid/GridProductFinder.php
@@ -6,41 +6,66 @@ namespace App\P011_LargestProductInGrid;
 
 class GridProductFinder
 {
+    private int $offset;
+
+    private int $rowLength;
+
+    private int $colLength;
+
     public function find(array $grid, int $numbers): int
     {
-        $rows = count($grid);
-        $cols = count($grid[0]);
-        //$offset = $numbers;
-        $offset = 2;
-        $product = 0;
+        $this->offset = $numbers;
+        $this->rowLength = count($grid) - 1;
+        $this->colLength = count($grid[0]);
 
-        //find product in each row
-//        foreach ($grid as $row) {
-//            for ($i = 0; $i + $offset <= $cols; $i++) {
-//                $tempProduct = array_product(array_slice($row, $i, $offset));
-//                if ($tempProduct > $product) {
-//                    $product = $tempProduct;
+        $this->findMaxProductInRows($grid);
+
+        $flatArray = array_merge(...$grid);
+
+        $this->findMaxProductInColumns($flatArray);
+
+        // find product in one diagonal
+
+//        for ($currentRowIndex = 0; $currentRowIndex + $offset < $rows + 1; $currentRowIndex++) {
+//            for ($currentColumnIndex = 0; $currentColumnIndex + $offset < $cols + 1; $currentColumnIndex++) {
+//                $diagonal = 1;
+//                foreach (range(0, $offset - 1) as $baseIndex) {
+//                    $diagonal *= $grid[$currentRowIndex + $baseIndex][$currentColumnIndex + $baseIndex];
+//                }
+//                if($diagonal > $product) {
+//                    $product = $diagonal;
 //                }
 //            }
 //        }
 
-        //find product in each column
-        $flatArray = call_user_func_array('array_merge', $grid);
-        $startKeys = array_map(fn($element) => $element * ($offset + 1), range(0, $offset - 1));
+        return 0;
+    }
 
-        for ($i = 0, $loopsMax = sizeof($flatArray) -1; $i + $offset < $loopsMax; $i++) {
-            $tempProduct = 1;
-            foreach ($startKeys as $startKey) {
-                $tempProduct *= $flatArray[$startKey];
+    private function findMaxProductInRows(array $array)
+    {
+        echo "Finding elements in each row:\n";
+        for ($rowCount = 0; $rowCount <= $this->rowLength; $rowCount++) {
+            for ($currentIndex = 0; $currentIndex + $this->offset <= $this->colLength; $currentIndex++) {
+                echo implode(', ', array_slice($array[$rowCount], $currentIndex, $this->offset)) . "\n";
             }
+        }
+        echo "---------------\n";
+    }
 
-            if($tempProduct > $product) {
-                $product = $tempProduct;
+    private function findMaxProductInColumns(array $array)
+    {
+        $lastIndex = count($array) - ($this->offset * ($this->colLength - 1));
+
+        echo "Finding elements in each row:\n";
+
+        for ($baseIndex = 0; $baseIndex <= $lastIndex; $baseIndex++) {
+            $collect = [];
+            for ($size = 0; $size < $this->offset; $size++) {
+                $collect[] = $array[$baseIndex + ($size * $this->colLength)];
             }
-
-            $startKeys = array_map(fn($element) => $element + 1, $startKeys);
+            echo implode(', ', $collect) . "\n";
         }
 
-        return $product;
+        echo "---------------\n";
     }
 }
