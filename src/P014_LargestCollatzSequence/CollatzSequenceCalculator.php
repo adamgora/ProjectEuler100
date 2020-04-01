66a9081642dcba0fef0ed98aa4e60283d2a307a6
Adam Góra <adamgora@icloud.com>
2020-04-01 18:45:19 +0200

    Basic setup

---
+++ src/P014_LargestCollatzSequence/CollatzSequenceCalculator.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\P014_LargestCollatzSequence;
+
+class CollatzSequenceCalculator
+{
+    public function getNumberWithLongestChainBelowLimit(int $limit): int
+    {
+    }
+}
---
+++ tests/P014_LargestCollatzSequence/CollatzSequenceCalculatorTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+use App\P014_LargestCollatzSequence\CollatzSequenceCalculator;
+use PHPUnit\Framework\TestCase;
+
+class CollatzSequenceCalculatorTest extends TestCase
+{
+    private CollatzSequenceCalculator $calculator;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->calculator = new CollatzSequenceCalculator();
+    }
+
+    /**
+     * @param array $numbers
+     * @param string $expectedResult
+     * @dataProvider cases
+     */
+    public function testSum(int $limit, int $expectedResult): void
+    {
+        $this->assertEquals(
+            $expectedResult,
+            $this->calculator->getNumberWithLongestChainBelowLimit($limit)
+        );
+    }
+
+    public function cases()
+    {
+        return [
+            [14, 9],
+            [5847, 3711],
+            [46500, 35655],
+            [54512, 52527],
+            [100000, 77031],
+        ];
+    }
+}
