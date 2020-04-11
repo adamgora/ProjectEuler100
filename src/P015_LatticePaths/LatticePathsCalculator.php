ba5b1a18ebf1a8c9d3bc44576400b856857904c4
Adam Góra <adamgora@icloud.com>
2020-04-11 17:48:12 +0200

    Basic files structure

---
+++ src/P015_LatticePaths/LatticePathsCalculator.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\P015_LatticePaths;
+
+class LatticePathsCalculator
+{
+    public function calculateNumberOfPossiblePaths(int $gridSize): int
+    {
+        return 0;
+    }
+}
