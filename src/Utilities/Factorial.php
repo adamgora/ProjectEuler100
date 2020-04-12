33b236dea85a1a371183a9940e5058aa2335e1d3
Adam Góra <adamgora@icloud.com>
2020-04-12 17:22:22 +0200

    Ecs

--- src/Utilities/Factorial.php
+++ src/Utilities/Factorial.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Utilities;
@@ -8,9 +9,10 @@ class Factorial
     public static function of(int $number): float
     {
         $factorial = 1;
-        for ($i = 1; $i <= $number; $i++) {
+        for ($i = 1; $i <= $number; ++$i) {
             $factorial *= $i;
         }
+
         return $factorial;
     }
 }
