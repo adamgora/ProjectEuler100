ab87cbf2bfbdf1c9b91a5c8654eb0ee253385bbf
Adam Góra <adamgora@icloud.com>
2020-04-19 08:39:20 +0200

    Created test and empty class

---
+++ src/P016_PowerDigitSum/PowerDigitSummarizer.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\P016_PowerDigitSum;
+
+class PowerDigitSummarizer
+{
+    public function summarize($base, $exponent): int
+    {
+        return 0;
+    }
+}
