db07005e516fbc17edca6237c1bef4bfde15c546
Adam Góra <adamgora@icloud.com>
2020-04-23 18:54:16 +0200

    Created basic class and tests

---
+++ src/P017_NumberLetterCounts/LetterCounter.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\P017_NumberLetterCounts;
+
+class LetterCounter
+{
+    public function countUpTo(int $limit): int
+    {
+        return 0;
+    }
+}
---
+++ tests/P017_NumberLetterCounts/LetterCounterTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use App\P017_NumberLetterCounts\LetterCounter;
+use PHPUnit\Framework\TestCase;
+
+class LetterCounterTest extends TestCase
+{
+    /**
+     * @var LetterCounter
+     */
+    private LetterCounter $counter;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->counter = new LetterCounter();
+    }
+
+    /**
+     * @param int $limit
+     * @param int $expectedResult
+     * @dataProvider cases
+     */
+    public function testCount(int $limit, int $expectedResult)
+    {
+        $this->assertEquals($this->counter->countUpTo($limit), $expectedResult);
+    }
+
+    public function cases()
+    {
+        return [
+            [5, 19],
+            [150, 1903],
+            [1000, 21124],
+        ];
+    }
+}
