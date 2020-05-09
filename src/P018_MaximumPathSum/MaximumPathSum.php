f3c58c663d2b37f8a1e1603f367dd90f6bf87814
Adam Góra <adamgora@icloud.com>
2020-05-09 07:40:47 +0200

    Solved problem 18

--- src/P018_MaximumPathSum/MaximumPathSum.php
+++ src/P018_MaximumPathSum/MaximumPathSum.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\P018_MaximumPathSum;
@@ -7,6 +8,22 @@ class MaximumPathSum
 {
     public function calculate(array $triangle): int
     {
-        return 0;
+        while (count($triangle) > 1) {
+            $triangleBaseRemoved = array_pop($triangle);
+            $currentTriangleSize = count($triangle);
+            $currentTriangleBase = $triangle[$currentTriangleSize - 1];
+
+            for ($i = 0, $iMax = count($currentTriangleBase) - 1; $i < $iMax; ++$i) {
+                if (0 === $currentTriangleBase[$i]) {
+                    break;
+                }
+
+                $currentTriangleBase[$i] += max($triangleBaseRemoved[$i], $triangleBaseRemoved[$i + 1]);
+            }
+
+            $triangle[$currentTriangleSize - 1] = $currentTriangleBase;
+        }
+
+        return $triangle[0][0];
     }
 }
