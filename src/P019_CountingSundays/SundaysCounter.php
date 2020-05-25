0272be2da8aa7b7524d787c4fe758e514bace6e2
Adam Góra <adamgora@icloud.com>
2020-05-25 16:34:56 +0200

    Basic test and class method

---
+++ src/P019_CountingSundays/SundaysCounter.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\P019_CountingSundays;
+
+class SundaysCounter
+{
+    public function countInRange(int $yearFrom, int $yearTo): int
+    {
+        return 0;
+    }
+}
