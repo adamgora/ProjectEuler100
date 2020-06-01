adea7a635c6d79c0143fe880fb9389604dfb38bc
Adam Góra <adamgora@icloud.com>
2020-06-01 16:43:35 +0200

    Created basic class and tests

---
+++ src/P020_FactorialDigitSum/FactorialSumCalculator.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\P020_FactorialDigitSum;
+
+class FactorialSumCalculator
+{
+    public function calculate(int $number): int
+    {
+        return 0;
+    }
+}
---
+++ tests/P020_FactorialDigitSum/FactorialSumCalculatorTest.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+use App\P020_FactorialDigitSum\FactorialSumCalculator;
+use PHPUnit\Framework\TestCase;
+
+class FactorialSumCalculatorTest extends TestCase
+{
+    private FactorialSumCalculator $calculator;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->calculator = new FactorialSumCalculator();
+    }
+
+    /**
+     * @param int $number
+     * @param int $expectedResult
+     * @dataProvider cases
+     */
+    public function testCalculate(int $number, int $expectedResult)
+    {
+        $this->assertEquals($expectedResult, $this->calculator->calculate($number));
+    }
+
+    public function cases()
+    {
+        return [
+            [10, 27],
+            [25,72],
+            [50, 216],
+            [75, 432],
+            [100, 648],
+        ];
+    }
+}
