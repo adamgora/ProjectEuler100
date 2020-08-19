741d8df4674f4ac8754caf231d201a0800725f8e
Adam Góra <adamgora@icloud.com>
2020-08-19 09:29:07 +0200

    Created test

---
+++ tests/P022_NamesScores/NameScoreSummarizerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use App\P022_NamesScores\NameScoreSummarizer;
+use PHPUnit\Framework\TestCase;
+
+class NameScoreSummarizerTest extends TestCase
+{
+    private NameScoreSummarizer $summarizer;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->summarizer = new NameScoreSummarizer();
+    }
+
+    /**
+     * @param array $testArray
+     * @param int $expectedResult
+     * @dataProvider cases
+     */
+    public function testSummarize(array $testArray, int $expectedResult): void
+    {
+        $this->assertEquals($expectedResult, $this->summarizer->getSum($testArray));
+    }
+
+    public function cases(): array
+    {
+        return [
+            [['THIS', 'IS', 'ONLY', 'A', 'TEST'], 791],
+            [['I', 'REPEAT', 'THIS', 'IS', 'ONLY', 'A', 'TEST'], 1468],
+        ];
+    }
+}
