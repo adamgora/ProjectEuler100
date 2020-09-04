4152c5f2b935bbc3fe2e768acf1cd4cbf054c259
Adam Góra <adamgora@icloud.com>
2020-09-04 13:51:23 +0200

    Basic class and test:

---
+++ src/P023_NonAbundantSums/NonAbundantSumCalculator.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\P023_NonAbundantSums;
+
+class NonAbundantSumCalculator
+{
+    public function getSum(int $limit): int
+    {
+        return 1;
+    }
+}
